59b0ef432efc1aa37cfb7b2a05b6c4c9c4b89285
xc <825351552@qq.com>
2020-06-05 19:49:54 +0800

    1

--- lodash/xstars-lodash.js
+++ lodash/xstars-lodash.js
@@ -70,4 +70,20 @@ var xstars = {
     }
     return array
   },
+  head: function (array) {
+    if (array.length == 0) {
+      return undefined
+    }
+    return array[0]
+  },
+  last: function (array) {
+    return array[array.length - 1]
+  },
+  initial: function (array) {
+    if (array.length <= 1) {
+      return []
+    }
+    return array.length = array.length - 1
+  }
+
 }
