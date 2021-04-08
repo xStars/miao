e734352547386acba992bd7503b3a781731dfb38
星辰 <stars@xingchendeMacBook-Pro.local>
2021-04-08 14:37:36 +0800

    review

--- lodash/xstars-lodash.js
+++ lodash/xstars-lodash.js
@@ -2,9 +2,16 @@ var xstars = {
   after: function (n, func) {
     let i = 0
     return (...args) => {
-      if (i++ > n) {
+      if (i++ >= n) {
         func(...args)
       }
+    }
+  },
+  ary: function (func, n = func.length) {
+    return (...args) => {
+      let limitArgs
+      args.length > n ? limitArgs = args.slice(0, n) : limitArgs = args
+      return func(...limitArgs)
     }
   }
 }
